37d582884462f9383a47ad3bdb62747089b49fbe
polandru <paulperdomo@comcast.net>
2023-07-01 23:14:25 -0500

    Assignment 8 includes

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -12,9 +12,12 @@
  */
 
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/init.h>
 #include <linux/printk.h>
 #include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>	
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
 #include "aesdchar.h"
